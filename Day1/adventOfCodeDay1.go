74871f5dd89e7310496ff3df45203212fd40b92f
dcottle-scottlogic <125259915+dcottle-scottlogic@users.noreply.github.com>
2025-12-01 10:19:23 +0000

    working method for D1P1

--- Day1/adventOfCodeDay1.go
+++ Day1/adventOfCodeDay1.go
@@ -1,5 +1,60 @@
 package adventOfCodeDay1
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//how many times does the dial point to 0
+
 func Day1Main() string {
-	return "Day 1 solution goes here."
+	file, err := os.Open("Day1/part1Input.txt")
+	if err != nil {
+		log.Fatalf("Could not open file: %v", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	location := 50
+	totalDefaults := 0
+	for scanner.Scan() {
+		var direction string
+		var clicks int
+		line := scanner.Text()
+		splints := strings.Split(line, "L")
+		if len(splints) > 1 {
+			direction = "Left"
+		} else {
+			direction = "Right"
+			splints = strings.Split(splints[0], "R")
+		}
+		clicks, error := strconv.Atoi(splints[1])
+		if error != nil {
+			log.Fatalf("Could not convert to integer: %v", error)
+		}
+		for i := 0; i < clicks; i++ {
+			if direction == "Left" {
+				location--
+			}
+			if direction == "Right" {
+				location++
+			}
+			if location <= -1 {
+				location = 99
+			}
+			if location >= 100 {
+				location = 0
+			}
+
+		}
+		if location == 0 {
+			totalDefaults++
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error occurred during scanning: %v", err)
+	}
+	return strconv.Itoa(totalDefaults)
 }
--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"fmt"
 
-	"example.com/AdventOfCode2025/mascot"
-
 	adventOfCodeDay1 "example.com/AdventOfCode2025/Day1"
 )
 
 func main() {
 	fmt.Println("Welcome to Advent of Code 2025!")
-	var ourMascot = mascot.GetMascot()
-	fmt.Printf("Our mascot is the %s a %s.\n", ourMascot.GetMascotName(), ourMascot.GetMascotType())
+	// var ourMascot = mascot.GetMascot()
+	// fmt.Printf("Our mascot is the %s a %s.\n", ourMascot.GetMascotName(), ourMascot.GetMascotType())
 	fmt.Println("----------------------")
 	fmt.Println(adventOfCodeDay1.Day1Main())
 }
