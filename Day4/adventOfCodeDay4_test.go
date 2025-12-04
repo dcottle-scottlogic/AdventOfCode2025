75432f7fbd8b2d15c35c94e3687e9007866ff1a8
dcottle-scottlogic <125259915+dcottle-scottlogic@users.noreply.github.com>
2025-12-04 12:05:45 +0000

    Small changes

--- Day4/adventOfCodeDay4_test.go
+++ Day4/adventOfCodeDay4_test.go
@@ -3,7 +3,7 @@ package adventOfCodeDay4
 import "testing"
 
 func TestDay3MainPart1(t *testing.T) {
-	got := Day4Main(true, "C:/Dev/AdventOfCode/AdventOfCode2025/Day4/testingdataInput.txt", 10)
+	got := Day4Main(true, "C:/Dev/AdventOfCode/AdventOfCode2025/Day4/testingInput.txt", 10)
 	want := 13
 	if got != want {
 		t.Errorf("Day2Main() = %v, want %v", got, want)
@@ -11,7 +11,7 @@ func TestDay3MainPart1(t *testing.T) {
 }
 
 func TestDay3MainPart2(t *testing.T) {
-	got := Day4Main(false, "C:/Dev/AdventOfCode/AdventOfCode2025/Day4/testingdataInput.txt", 10)
+	got := Day4Main(false, "C:/Dev/AdventOfCode/AdventOfCode2025/Day4/testingInput.txt", 10)
 	want := 43
 	if got != want {
 		t.Errorf("Day2Main() = %v, want %v", got, want)
